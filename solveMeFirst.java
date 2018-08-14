97c5251722ea5a3eeb10d3c96f619bae05431a96
Rishabh Nevatia <nevatia.rishabh@gmail.com>
2018-08-14 22:04:41 +0530

    My first Hackerrank code

---
+++ solveMeFirst.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/**
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Function prototype:
+
+int solveMeFirst(int a, int b);
+
+where,
+
+a is the first integer input.
+b is the second integer input
+Return values
+
+sum of the above two integers
+*/
+public class Solution {
+
+
+
+    static int solveMeFirst(int a, int b) {
+      // Hint: Type return a+b; below
+      
+	}
+
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+	}
+}
